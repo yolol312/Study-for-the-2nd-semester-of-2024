e78661450512641d43a90ca948dad75831df48e0
Seongmo Ahn <djgood780@gmail.com>
2024-09-06 14:38:51 +0900

    자바 구구단 이용스 ì작ì

---
+++ src/main_08_01.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class main_08_01 {
+    public void gugu(int num) {
+        for (int i = 1; i < 10; i++) {
+            System.out.printf("%d * %d = %d%n", num, i, num * i);
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        InputStream in = System.in;
+        InputStreamReader reader = new InputStreamReader(in);
+        BufferedReader br = new BufferedReader(reader);
+        int num = Integer.parseInt(br.readLine());
+
+        main_08_01 main = new main_08_01();
+        main.gugu(num);
+    }
+}
