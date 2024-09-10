585c730481f7d1bb40f6149127454fc041934752
Seongmo Ahn <djgood780@gmail.com>
2024-09-10 15:25:55 +0900

    Create findBySubjectLike Method

--- src/main/java/com/example/sbb/QuestionRepository.java
+++ src/main/java/com/example/sbb/QuestionRepository.java
@@ -3,8 +3,11 @@ package com.example.sbb;
 import com.example.sbb.DTO.QuestionDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface QuestionRepository extends JpaRepository<QuestionDTO, Integer> {
     QuestionDTO findBySubject(String subject);
     QuestionDTO findBySubjectAndContent(String subject, String content);
+    List<QuestionDTO> findBySubjectLike(String subject);
 }
 
--- src/test/java/com/example/sbb/SbbApplicationTests.java
+++ src/test/java/com/example/sbb/SbbApplicationTests.java
@@ -19,15 +19,23 @@ public class SbbApplicationTests {
 
     @Test
     void testJpa() {
+        //findByID
         Optional<QuestionDTO> oq = this.questionRepository.findById(1);
         if(oq.isPresent()){
             QuestionDTO q = oq.get();
             assertEquals("sbb가 무엇인가요?", q.getSubject());
         }
+        //findBySubject
         QuestionDTO oq2 = this.questionRepository.findBySubject("sbb가 무엇인가요?");
         assertEquals(1, oq2.getId());
 
+        //findBySubjectAndContent
         QuestionDTO oq3 = this.questionRepository.findBySubjectAndContent("sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
         assertEquals(1, oq3.getId());
+
+        //findBySubjectLike
+        List<QuestionDTO> oq4 = this.questionRepository.findBySubjectLike("sbb%");
+        QuestionDTO question = oq4.get(0);
+        assertEquals("sbb에 대해서 알고 싶습니다.", question.getContent());
     }
 }
