584f41696c42abb8c9871d3f9986f2225182ffa3
Seongmo Ahn <djgood780@gmail.com>
2024-09-10 15:35:53 +0900

    Create update data code

--- src/test/java/com/example/sbb/SbbApplicationTests.java
+++ src/test/java/com/example/sbb/SbbApplicationTests.java
@@ -11,6 +11,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 public class SbbApplicationTests {
@@ -19,6 +20,20 @@ public class SbbApplicationTests {
 
     @Test
     void testJpa() {
+        /*insert data
+        QuestionDTO q1 = new QuestionDTO();
+        q1.setSubject("sbb가 무엇인가요?");
+        q1.setContent("sbb에 대해서 알고 싶습니다.");
+        q1.setCreateDate(LocalDateTime.now());
+        this.questionRepository.save(q1);  // 첫번째 질문 저장
+
+        QuestionDTO q2 = new QuestionDTO();
+        q2.setSubject("스프링부트 모델 질문입니다.");
+        q2.setContent("id는 자동으로 생성되나요?");
+        q2.setCreateDate(LocalDateTime.now());
+        this.questionRepository.save(q2);  // 두번째 질문 저장
+        */
+
         //findByID
         Optional<QuestionDTO> oq = this.questionRepository.findById(1);
         if(oq.isPresent()){
@@ -37,5 +52,12 @@ public class SbbApplicationTests {
         List<QuestionDTO> oq4 = this.questionRepository.findBySubjectLike("sbb%");
         QuestionDTO question = oq4.get(0);
         assertEquals("sbb에 대해서 알고 싶습니다.", question.getContent());
+
+        //update data
+        Optional<QuestionDTO> oq5 = this.questionRepository.findById(1);
+        assertTrue(oq5.isPresent());
+        QuestionDTO q = oq5.get();
+        q.setSubject("수정된 제목");
+        this.questionRepository.save(q);
     }
 }
