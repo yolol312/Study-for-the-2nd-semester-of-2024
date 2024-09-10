3bd70df138cdab3112fdfa141384375aee15faf5
Seongmo Ahn <djgood780@gmail.com>
2024-09-10 16:03:45 +0900

    Create add answer data, select answer datas By question code

--- src/test/java/com/example/sbb/SbbApplicationTests.java
+++ src/test/java/com/example/sbb/SbbApplicationTests.java
@@ -2,10 +2,12 @@ package com.example.sbb;
 
 import java.util.Optional;
 
+import com.example.sbb.DTO.AnswerDTO;
 import com.example.sbb.DTO.QuestionDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -18,9 +20,14 @@ public class SbbApplicationTests {
     @Autowired
     private QuestionRepository questionRepository;
 
+    @Autowired
+    private AnswerRepository answerRepository;
+
+    @Transactional //DB 섹션 유지
     @Test
     void testJpa() {
-        /*insert data
+        /*
+        //insert data
         QuestionDTO q1 = new QuestionDTO();
         q1.setSubject("sbb가 무엇인가요?");
         q1.setContent("sbb에 대해서 알고 싶습니다.");
@@ -32,13 +39,12 @@ public class SbbApplicationTests {
         q2.setContent("id는 자동으로 생성되나요?");
         q2.setCreateDate(LocalDateTime.now());
         this.questionRepository.save(q2);  // 두번째 질문 저장
-        */
 
         //findByID
         Optional<QuestionDTO> oq = this.questionRepository.findById(1);
         if(oq.isPresent()){
-            QuestionDTO q = oq.get();
-            assertEquals("sbb가 무엇인가요?", q.getSubject());
+            QuestionDTO q3 = oq.get();
+            assertEquals("sbb가 무엇인가요?", q3.getSubject());
         }
         //findBySubject
         QuestionDTO oq2 = this.questionRepository.findBySubject("sbb가 무엇인가요?");
@@ -50,14 +56,44 @@ public class SbbApplicationTests {
 
         //findBySubjectLike
         List<QuestionDTO> oq4 = this.questionRepository.findBySubjectLike("sbb%");
-        QuestionDTO question = oq4.get(0);
-        assertEquals("sbb에 대해서 알고 싶습니다.", question.getContent());
+        QuestionDTO q4 = oq4.get(0);
+        assertEquals("sbb에 대해서 알고 싶습니다.", q4.getContent());
 
         //update data
         Optional<QuestionDTO> oq5 = this.questionRepository.findById(1);
         assertTrue(oq5.isPresent());
-        QuestionDTO q = oq5.get();
-        q.setSubject("수정된 제목");
-        this.questionRepository.save(q);
+        QuestionDTO q5 = oq5.get();
+        q5.setSubject("수정된 제목");
+        this.questionRepository.save(q5);
+
+        //delete data
+        Optional<QuestionDTO> oq6 = this.questionRepository.findById(1);
+        assertTrue(oq6.isPresent());
+        QuestionDTO q6 = oq6.get();
+        this.questionRepository.delete(q6);
+        assertEquals(1, this.questionRepository.count());
+
+
+        //add answer data
+        Optional<QuestionDTO> oq7 = this.questionRepository.findById(2);
+        assertTrue(oq7.isPresent());
+        QuestionDTO q7 = oq7.get();
+
+        AnswerDTO a = new AnswerDTO();
+        a.setContent("네 자동으로 생성됩니다.");
+        a.setQuestion(q7);
+        a.setCreateDate(LocalDateTime.now());
+        this.answerRepository.save(a);
+        */
+
+        //select answer datas By question
+        Optional<QuestionDTO> oq = this.questionRepository.findById(2);
+        assertTrue(oq.isPresent());
+        QuestionDTO q = oq.get();
+        List<AnswerDTO> answerList = q.getAnswerList();
+
+
+        assertEquals(1, answerList.size());
+        assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
     }
 }
